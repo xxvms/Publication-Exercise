81188025bc39cf0229b7b2c75db970e3f9f41005
Tom Breza <tom@pcservicegroup.co.uk>
2017-05-25 16:54:39 +0100

    Add project files.

---
+++ Publication-Exercise/Publication-Exercise.cpp
@@ -0,0 +1,94 @@
+// Publication-Exercise.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+#include <string>
+
+class Publication
+{
+	std::string title;
+	double price;
+
+public:
+	Publication() : title("n/a"), price(0.0)
+	{}
+
+	void getData()
+	{
+		std::cout << "Please provide title: ";
+		std::cin >> title;
+		std::cout << "Please provide $$: ";
+		std::cin >> price;
+	}
+	void putData()
+	{
+		std::cout << "Book title: " << title << " Book cost: $" << price << std::endl;
+	}
+};
+class Book : public Publication
+{
+	int pageCount;
+
+public:
+	Book() : pageCount(0)
+	{}
+	void getData()
+	{
+		std::cout << "Please provide page count: ";
+		std::cin >> pageCount;
+	}
+	void putData()
+	{
+		std::cout << "Number of pages: " << pageCount << std::endl;
+	}
+};
+class Tape : public Publication
+{
+	double tapeTime;
+public:
+	Tape() : tapeTime(0.0)
+	{}
+	void getData()
+	{
+		std::cout << "How long is the tape in minutes: ";
+		std::cin >> tapeTime;
+	}
+	void putData()
+	{
+		std::cout << "Lenght of the tape in minutes: " << tapeTime << std::endl;
+	}
+};
+
+
+int main()
+{
+
+	std::cout << "Please provide" << std::endl;
+	Book bk1;
+	Tape tp1;
+
+	std::cout << "Book: " << std::endl;
+	bk1.Publication::getData();
+	bk1.getData();
+
+	std::cout << "Tape: " << std::endl;
+	tp1.Publication::getData();
+	tp1.getData();
+
+	std::cout << "This is what you have done!!" << std::endl;
+
+	std::cout << "For book " << std::endl;
+	bk1.Publication::putData();
+	bk1.putData();
+
+	std::cout << "For tape " << std::endl;
+
+	tp1.Publication::putData();
+	tp1.putData();
+
+
+	system("pause");
+    return 0;
+}
+
